2e4191d61e7a1294b3b402a2906e91349d7f401d
Dmitry White <dmitry.b.white@gmail.com>
2021-10-06 14:39:21 +0100

    Add VPC example update automation

--- vpc-example/bootstrap.sh
+++ vpc-example/bootstrap.sh
@@ -5,6 +5,10 @@ PARAMETERS="parameters"
 STACK_NAME="vpc-example"
 PROFILE="DmitryPC"
 
+CHANGE_SET_NAME="vpc-example-update"
+TEMPLATE_UPDATED="template-updated"
+PARAMETERS_UPDATED="parameters-updated"
+
 create() {
   echo "Creating a VPC Example stack with File parameters..."
 
@@ -31,6 +35,67 @@ observe() {
     --profile $PROFILE
 
   echo "Observation of the VPC Example stack completed!"
+}
+
+
+prepareUpdate() {
+  echo "Preparing update for the VPC Example stack..."
+
+  aws cloudformation create-change-set \
+    --stack-name $STACK_NAME \
+    --change-set-name $CHANGE_SET_NAME \
+    --template-body "file://${TEMPLATE_UPDATED}.yml" \
+    --parameters "file://${PARAMETERS_UPDATED}.json" \
+    --profile $PROFILE
+
+  echo "Waiting for VPC Example stack to complete update preparation..."
+
+  aws cloudformation wait change-set-create-complete \
+    --stack-name $STACK_NAME \
+    --change-set-name $CHANGE_SET_NAME \
+    --profile $PROFILE
+
+  echo "Update of VPC Example stack prepared!"
+}
+
+viewUpdate() {
+  echo "Retrieving updates for the VPC Example stack..."
+
+  aws cloudformation list-change-sets \
+    --stack-name $STACK_NAME \
+    --profile $PROFILE
+
+  echo "Retrieving update details for the VPC Example stack..."
+
+  aws cloudformation describe-change-set \
+    --stack-name $STACK_NAME \
+    --change-set-name $CHANGE_SET_NAME \
+    --output table \
+    --profile $PROFILE
+
+  echo "Updates of VPC Example stack retrieved!"
+}
+
+update() {
+  prepareUpdate
+  viewUpdate
+}
+
+applyUpdate() {
+  echo "Applying update for the VPC Example stack..."
+
+  aws cloudformation execute-change-set \
+    --stack-name $STACK_NAME \
+    --change-set-name $CHANGE_SET_NAME \
+    --profile $PROFILE
+
+  echo "Waiting for VPC Example stack to complete update..."
+
+  aws cloudformation wait stack-update-complete \
+    --stack-name $STACK_NAME \
+    --profile $PROFILE
+
+  echo "Update of VPC Example stack completed!"
 }
 
 
@@ -61,6 +126,12 @@ while [ $# -gt 0 ]; do      # Wait until there are no arguments
     ;; # End of one of the cases
   check)
     command="observe"
+    ;;
+  reboot)
+    command="update"
+    ;;
+  apply)
+    command="applyUpdate"
     ;;
   stop)
     command="delete"
